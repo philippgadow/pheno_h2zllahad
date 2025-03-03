4972cd57f8cd4a252b490d4c32b3b9be5454d3d5
Philipp Gadow <paul.philipp.gadow@cern.ch>
2025-03-03 15:06:54 +0100

    micromamba

--- setup.sh
+++ setup.sh
@@ -1,19 +1,65 @@
 #!/bin/bash
 
-# check if conda exists, otherwise install miniconda
-if ! conda --version >/dev/null 2>&1; then
-    echo 'Error: conda is not installed.' >&2
-    echo 'Installing miniconda...'
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    bash Miniconda3-latest-Linux-x86_64.sh -b
-    rm Miniconda3-latest-Linux-x86_64.sh
-    export PATH="$HOME/miniconda3/bin:$PATH"
+# Check if micromamba is installed
+if [ ! -f ./bin/micromamba ]; then
+    echo "micromamba could not be found, installing micromamba..."
+    mkdir -p ./bin
+
+    OS=$(uname -s)
+    ARCH=$(uname -m)
+
+    echo "OS: $OS"
+    echo "ARCH: $ARCH"
+
+    if [ "$OS" = "Linux" ]; then
+        if [ "$ARCH" = "x86_64" ]; then
+            URL="https://micro.mamba.pm/api/micromamba/linux-64/latest"
+        elif [ "$ARCH" = "aarch64" ]; then
+            URL="https://micro.mamba.pm/api/micromamba/linux-aarch64/latest"
+        elif [ "$ARCH" = "ppc64le" ]; then
+            URL="https://micro.mamba.pm/api/micromamba/linux-ppc64le/latest"
+        else
+            echo "Unsupported architecture: $ARCH"
+            exit 1
+        fi
+    elif [ "$OS" = "Darwin" ]; then
+        if [ "$ARCH" = "x86_64" ]; then
+            URL="https://micro.mamba.pm/api/micromamba/osx-64/latest"
+        elif [ "$ARCH" = "arm64" ]; then
+            URL="https://micro.mamba.pm/api/micromamba/osx-arm64/latest"
+        else
+            echo "Unsupported architecture: $ARCH"
+            exit 1
+        fi
+    else
+        echo "Unsupported operating system: $OS"
+        exit 1
+    fi
+
+    curl -Ls $URL | tar -xvj -C ./ bin/micromamba
 fi
 
-if ! conda env list | grep -q "h2zllahad"; then
-    conda create -y --name h2zllahad python=3.9
+export MAMBA_ROOT_PREFIX=$PWD/micromamba
+export MAMBA_EXE=$PWD/bin/micromamba
+
+# Initialize shell correctly
+if [ -n "$ZSH_VERSION" ]; then
+    echo "Initializing micromamba for zsh"
+    eval "$($MAMBA_EXE shell hook --shell zsh)"
+elif [ -n "$BASH_VERSION" ]; then
+    echo "Initializing micromamba for bash"
+    eval "$($MAMBA_EXE shell hook --shell bash)"
+else
+    echo "Unsupported shell"
+    exit 1
 fi
 
-conda activate h2zllahad
-    conda install -y conda-forge::subversion conda-forge::fortran-compiler
-    conda install -y conda-forge::pythia8 conda-forge::hepmc2 conda-forge::hepmc3 conda-forge::lhapdf
+# Create environment if it doesn't exist
+if ! $MAMBA_EXE env list | grep -q "micromamba_h2zllahad"; then
+    $MAMBA_EXE create -y --name micromamba_h2zllahad python=3.9
+fi
+
+# Activate environment properly
+micromamba activate micromamba_h2zllahad
+micromamba install -y conda-forge::subversion conda-forge::fortran-compiler
+micromamba install -y conda-forge::pythia8 conda-forge::hepmc2 conda-forge::hepmc3 conda-forge::lhapdf
